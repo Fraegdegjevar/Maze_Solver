c1842d177c8f6f84b557a48c4c7152b8425d6bbc
Fraegdegjevar <willhcode@gmail.com>
2025-04-29 16:28:27 +0100

    renamed window.py to graphics.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 # Created by Will Harvey 29/04/2025
-from window import Window
+from window import *
 
 def main():
     win = Window(800,600)
     win.wait_for_close()
-    
+
+if __name__ == "__main__":
+    main()
