6b783edea974a36ea07385cf601b402a587d0aba
Fraegdegjevar <willhcode@gmail.com>
2025-04-29 16:17:50 +0100

    Added src/ folder structure. Created window.py containing Window class. Added main.py to hook into an instance of Window().

--- main.py
+++
@@ -1,4 +0,0 @@
-# Created by Will Harvey 29/04/2025
-
-def main():
-    pass
---
+++ src/main.py
@@ -0,0 +1,7 @@
+# Created by Will Harvey 29/04/2025
+from window import Window
+
+def main():
+    win = Window(800,600)
+    win.wait_for_close()
+    
