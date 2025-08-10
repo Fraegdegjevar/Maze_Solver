4623fea9408a5603891f8525ae5688dce539704d
Fraegdegjevar <willhcode@gmail.com>
2025-08-10 16:10:50 +0100

    Added constructor and basic methods to maze.py

--- src/maze.py
+++ src/maze.py
@@ -1,3 +1,16 @@
 class Maze:
-    def __init__(self):
+    def __init__(
+        self, x1, y1,
+        num_rows, num_cols,
+        cell_size_x, cell_size_y,
+        win):
+        pass
+
+    def _create_cells(self):
+        pass
+    
+    def _draw_cell(self, i, j):
+        pass
+    
+    def _animate(self):
         pass
